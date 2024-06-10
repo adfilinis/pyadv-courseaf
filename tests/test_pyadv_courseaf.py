be83f7ea9c36d7bfa4a7a72f50a5822c2ee67eea
adfilinis <adrianos.filinis@unibe.ch>
2024-06-10 14:41:11 +0200

    added test second time

--- tests/test_pyadv_courseaf.py
+++ tests/test_pyadv_courseaf.py
@@ -1,7 +1,7 @@
 from pyadv_courseaf import algos
 
 def test_add_one():
-    out = algos.add_one(2)
+    out = algos.add_one(1)
     assert out == 2, f'output should be 2 but is {out}'
 
 def test_add_two():
